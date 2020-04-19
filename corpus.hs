3dceb3c76ca14b6f208d574c2881a8fd540fd334
amg-12 <git@amitai.ga>
2020-04-19 19:34:31 +0100

    didn't work well

--- corpus.hs
+++ corpus.hs
@@ -1,18 +1,42 @@
 import System.IO
+import System.IO.Unsafe -- shh
 import Control.Monad
+import System.Random.Pick
+import Data.List
 import Data.List.Split
+import qualified Data.Set as Set
 import qualified Data.SetMap as Map
 
 
 corpusPath :: String
 corpusPath = "browntag_nolines.txt"
 
-corpus :: IO String
-corpus = join $ fmap hGetContents $ openFile corpusPath ReadMode
+corpus :: String
+corpus = unsafePerformIO $ join $ fmap hGetContents $ openFile corpusPath ReadMode
+-- unsafe because i need to memoize it and also don't want fmap everywhere
 
-tagMap :: [String] -> Map.SetMap String String
-tagMap []     = Map.empty
-tagMap (x:xs) = Map.insert p w $ tagMap xs
-  where pw = splitOn "_" x
-        w = head pw
-        p = head $ tail pw
+word, part :: String -> String
+word =        takeWhile (/='_')
+part = tail . dropWhile (/='_')
+
+partMap :: Map.SetMap String String
+partMap = (m . words) corpus where
+  m []     = Map.empty
+  m (x:xs) = Map.insert (part x) (word x) $ m xs
+
+parts :: [String]
+parts = Map.keys partMap
+
+getWords :: String -> [String]
+getWords x = Set.elems $ Map.lookup x partMap
+
+getWord :: String -> IO String
+getWord = pickOne . getWords
+
+sub :: String -> IO String
+sub = getWord . part
+
+test = do
+  x <- pickOne $ lines $ corpus
+  return $ intercalate " " $ map (unsafePerformIO . sub) $ words x
+-- bad
